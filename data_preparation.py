2653b18272b3877a6d8117b9a7330c3439d3c0dd
AlexeyRau <arau729@gmail.com>
2026-01-10 03:05:53 +0600

    feat: implement data cleaning and preprocessing

--- data_preparation.py
+++ data_preparation.py
@@ -4,16 +4,20 @@ import os
 
 print("Скачиваем датасет TMDB...")
 path = kagglehub.dataset_download("tmdb/tmdb-movie-metadata")
-print("Путь:", path)
-print("Файлы:", os.listdir(path))
 
 movies_path = os.path.join(path, "tmdb_5000_movies.csv")
 df = pd.read_csv(movies_path)
 
 print(f"\nУспешно загружено {len(df)} строк.")
-print("\nПервые 5 строк:")
-print(df.head())
-print("\nИнформация о датасете:")
-print(df.info())
-print("\nПроверка пропущенных значений:")
-print(df.isnull().sum())
+
+df_simple = df[['title', 'overview', 'release_date', 'runtime']].copy()
+
+df_simple = df_simple.dropna(subset=['overview'])
+df_simple = df_simple[df_simple['overview'].str.strip() != '']
+
+df_simple['release_date'] = pd.to_datetime(df_simple['release_date'], errors='coerce')
+df_simple['year'] = df_simple['release_date'].dt.year.fillna(0).astype(int)
+
+df_simple.reset_index(drop=True, inplace=True)
+df_simple.to_csv('movies_simple.csv', index=False)
+print(f"Сохранено {len(df_simple)} фильмов")
